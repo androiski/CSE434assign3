95983ec8a975f01bbb84eadd18ebd0819e1f07b1
Andrew <androiski@gmail.com>
2019-10-28 00:43:32 -0700

    fingers crossed

--- udp_client.c
+++ udp_client.c
@@ -10,7 +10,23 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#include "msgstruct.h"
+#include <time.h>
+#include <stdint.h>
+#include <limits.h>
+
+typedef struct{
+
+    uint8_t firstnm; //first name 
+    uint8_t lastnm; //last name 
+    uint8_t opcode; //opcode
+    struct tm timepost; //timepost
+    uint8_t length; //number of char to send
+    char memo[201]; //msg to send
+
+}msgstruct;
+
+void resetmsg(msgstruct *m);
+
 
 int main() {
 
@@ -18,6 +34,16 @@ int main() {
     int sockfd;
     struct sockaddr_in servaddr;
     char send_buffer[1024];
+    char restrict_buffer[1024];
+
+    char posthash[5] = {'p', 'o', 's', 't', '#'};
+    char retcmd[11] = {'r', 'e', 't', 'r', 'i', 'e', 'v', 'e', '#', '\n', '\0'};
+    int posterrct = 0;
+    int recerrct = 0;
+    msgstruct msg;
+    time_t _time;
+    int msgct = 0;
+
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
@@ -46,6 +72,22 @@ int main() {
     // datagram is sent. After that, you can call send() and recv() instead
     // of sendto() and recvfrom().
 
+/* 
+stole this snippet from assign 1 udp_client.c 
+probably need to change some stuff
+*/
+
+    ret = connect(sockfd,
+                    (struct sockaddr *) &servaddr,
+                    sizeof(servaddr));
+
+    if (ret < 0 ){
+        printf("connect() error: %s.\n", strerror(errno));
+        return -1;
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////////
+    
     while (1) {
 
         // The fgets() function read a line from the keyboard (i.e, stdin)
@@ -55,6 +97,73 @@ int main() {
             stdin);
 
         // TODO: Check the user input format.
+        resetmsg(&msg);
+        posterrct = 0;
+        recerrct = 0;
+
+        //compares first 5 char from send_buffer to 'post#'
+        for (int i = 0; i < 5 && !posterrct; i++){
+            if(send_buffer[i] != posthash[i]){
+                posterrct = send_buffer[i] - posthash[i];
+            }
+        }
+        if(send_buffer[5] == '\n'){
+            posterrct = INT_MAX;
+        }
+
+        //compares the first 11 char from send_buffer to see if 'retrieve#'
+        for(int i = 0; i < 11 && !recerrct; i++){
+            if(send_buffer[i] != retcmd[i]){
+                recerrct = send_buffer[i] - retcmd[i];
+            }
+        }
+
+        if(posterrct == 0 && recerrct != 0){
+            int j = 5;
+            while(send_buffer[j] != '\n'){
+                j+=1;
+            }
+            msg.length = (uint8_t)j + 1;
+            time(&_time);
+            msg.timepost = *localtime(&_time);
+            msg.firstnm = (uint8_t)'A';
+            msg.lastnm = (uint8_t)'M';
+            msg.opcode = (uint8_t)1;
+            for(int i = 0; msg.length; i++){
+                msg.memo[i] = send_buffer[5+i];
+            }
+
+            sendto(sockfd, &msg, sizeof(msgstruct), 0, (struct sockaddr *) & servaddr, sizeof(servaddr));
+            recv(sockfd, &msg, sizeof(msgstruct), 0);
+
+            if(msg.opcode == 2 && msg.firstnm == (uint8_t)'A' && msg.lastnm == (uint8_t)'M'){
+                printf("post_ack#successful\n");
+            }
+            else if(posterrct != 0 && recerrct == 0){
+                if(msgct != 0){
+                    time(&_time);
+                    msg.timepost = *localtime(&_time);
+                    msg.opcode = (uint8_t)3;
+                    msg.firstnm = (uint8_t)'A';
+                    msg.lastnm = (uint8_t)'M';
+
+                    sendto(sockfd, &msg, sizeof(msgstruct), 0 , (struct sockaddr *) & servaddr, sizeof(servaddr));
+                    recv(sockfd, &msg, sizeof(msgstruct), 0);
+
+                    if(msg.opcode == 4 && msg.firstnm == (uint8_t)'A' && msg.lastnm == (uint8_t)'M'){
+                        printf("retrieve_ack#%s", msg.memo);
+                    }
+                }
+            }
+            else{
+                printf("Error: Unrecognized command format\n");
+            }
+            for(int i = 0; i < sizeof(send_buffer); i++){
+                send_buffer[i] = '\0';
+            }
+        }
+
+
 
         // The sendto() function send the designated number of bytes in the
         // "send_buffer" to the destination address.
@@ -72,9 +181,19 @@ int main() {
 
         // TODO: You are supposed to call the recvfrom() function here.
         // The client will receive the acknowledgement from the server.
-
     }
-
     return 0;
 }
 
+void resetmsg(msgstruct * msg){
+        time_t actualtime;
+        msg->firstnm = (uint8_t)'A';
+        msg->lastnm = (uint8_t)'M';
+        msg->opcode = (uint8_t)0;
+        time(&actualtime);
+        msg->timepost = *localtime(&actualtime);
+        msg->length = (uint8_t)0;
+        for(int i = 0; i < 201; i++){
+            msg->memo[i] = '\0';
+        }
+};
--- udp_server.c
+++ udp_server.c
@@ -10,20 +10,37 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include <time.h>
+#include <stdint.h>
+#include <limits.h>
+
+typedef struct{
+
+    uint8_t firstnm; //first name 
+    uint8_t lastnm; //last name 
+    uint8_t opcode; //opcode
+    struct tm timepost; //timepost
+    uint8_t length; //number of char to send
+    char memo[201]; //msg to send
+
+}msgstruct;
+
 int main() {
 
     int ret;
     int sockfd;
     struct sockaddr_in servaddr, cliaddr;
     char recv_buffer[1024];
-    int recv_len;
     socklen_t len;
 
+    //added myself
+    msgstruct msg;
+    msgstruct recent;
+    time_t _time;
+    struct tm svrtime;
+    FILE *log;
+    ////
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        printf("socket() error: %s.\n", strerror(errno));
-        return -1;
-}
 
     // The servaddr is the address and port number that the server will 
     // keep receiving from.
@@ -32,22 +49,56 @@ int main() {
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(32000);
 
+    memset(&cliaddr, 0, sizeof(cliaddr));
+    cliaddr.sin_family = AF_INET;
+    
     bind(sockfd, 
          (struct sockaddr *) &servaddr, 
          sizeof(servaddr));
 
     while (1) {
-        len = sizeof(cliaddr);
-        recv_len = recvfrom(sockfd, // socket file descriptor
-                 recv_buffer,       // receive buffer
-                 sizeof(recv_buffer),  // max number of bytes to be received
-                 0,
-                 (struct sockaddr *) &cliaddr,  // client address
-                 &len);             // length of client address structure
+        memset(&recv_buffer, 0, sizeof(recv_buffer));
+        recvfrom(sockfd, &msg, sizeof(msg), 0, (struct sockaddr *) &cliaddr, &len);
 
-        if (recv_len <= 0) {
-            printf("recvfrom() error: %s.\n", strerror(errno));
-            return -1;
+        if(msg.opcode == 1 && msg.firstnm == (uint8_t)'A' && msg.lastnm == (uint8_t)'M'){
+            time(&_time);
+            svrtime = *localtime(&_time);
+
+            log = fopen("serverlog.txt", "a");
+            fprintf(log, "<%d:%d:%d> [%s:%hu] post#%s", msg.timepost.tm_hour,msg.timepost.tm_min,msg.timepost.tm_sec,inet_ntoa(cliaddr.sin_addr),
+                        cliaddr.sin_port,msg.memo);
+            fprintf(log, log, "<%d:%d:%d> [%s:%hu] post_ack#successful\n", svrtime.tm_hour,svrtime.tm_min,svrtime.tm_sec,inet_ntoa(cliaddr.sin_addr),
+                        cliaddr.sin_port);
+            fflush(log);
+            fclose(log);
+
+            for(int i = 0; msg.length; i++){
+                msg.memo[i] = recent.memo[i];
+            }
+            recent.length = msg.length;
+            msgstruct ack;
+            ack.opcode = 2;
+            ack.firstnm = (uint8_t)'A';
+            ack.lastnm = (uint8_t)'M'; 
+            
+            sendto(sockfd, &ack, sizeof(msgstruct), 0, (struct sockaddr *) &cliaddr, sizeof(cliaddr));
+        }
+        else if(msg.opcode == 3 && msg.firstnm == (uint8_t)'A' && msg.lastnm == (uint8_t)'M'){
+            time(&_time);
+            svrtime = *localtime(&_time);
+            recent.opcode = 4;
+            recent.firstnm = (uint8_t)'A';
+            recent.lastnm = (uint8_t)'M'; 
+
+            log = fopen("serverlog.txt", "a");
+            fprintf(log, "<%d:%d:%d> [%s:%hu] retrieve#\n", msg.timepost.tm_hour,msg.timepost.tm_min,msg.timepost.tm_sec,inet_ntoa(cliaddr.sin_addr),
+                        cliaddr.sin_port);
+            fprintf(log, log, "<%d:%d:%d> [%s:%hu] retrieve#%s", svrtime.tm_hour,svrtime.tm_min,svrtime.tm_sec,inet_ntoa(cliaddr.sin_addr),
+                        cliaddr.sin_port, recent.memo);
+            fflush(log);
+            fclose(log);
+
+            sendto(sockfd, &recent, sizeof(msgstruct), 0, (struct sockaddr *) &cliaddr, sizeof(cliaddr));
         }
 
       // TODO: check whether the recv_buffer contains a proper header
